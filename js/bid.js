84a9648fecaa182cb2e899b0ee3c51612a9b3414
tales00 <talesworks@gmail.com>
2015-05-05 14:49:46 +0800

    強化倒數日期計算的功能，未來可對應動態連續載入的設計

--- js/bid.js
+++ js/bid.js
@@ -2,11 +2,17 @@
     window.location = "./biddetail.aspx?id=" + index;
 }
 
-($(function() {
-    // 計算倒數計時，使用 jquery.timers.js
+// 倒數時日計算，使用 jquery.timers.js
+// 讀取 .timeLeft span:not(.counted) 中的日期資料並轉換成倒數時間
+// 因為最後會把標籤加上 counted 的 class
+// 所以重複執行並不會把轉換過的再做一次轉換，造成錯誤。
+// 之後若是要改成動態連續載入時，請在載入完成後，
+// 重新呼叫一次這個函數即可。
+
+function endDateCalc() {
     var startDate = new Date();
 
-    $('.timeLeft span').each(function() {
+    $('.timeLeft span:not(.counted)').each(function() {
         var endDate = new Date($(this).text());
         var spantime = (endDate - startDate) / 1000;
         var countText = '';
@@ -29,6 +35,12 @@
             countText = '此競標已結束';
         }
 
-        $(this).text(countText);
+        $(this).text(countText).addClass('counted');
     });
+}
+
+($(function() {
+    // 執行倒數時日計算
+    endDateCalc();
+
 }));
